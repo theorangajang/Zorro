b7733a58467a2a7a78fb2530b7e1b586162c21a9
Alex Jang <raymond.jang@ibm.com>
2020-06-18 14:19:12 -0500

    Initial Commit

---
+++ Zorro/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Zorro
+//
+//  Created by Alex Jang on 6/18/20.
+//  Copyright © 2020 Alex Jang. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ ZorroTests/ZorroTests.swift
@@ -0,0 +1,34 @@
+//
+//  ZorroTests.swift
+//  ZorroTests
+//
+//  Created by Alex Jang on 6/18/20.
+//  Copyright © 2020 Alex Jang. All rights reserved.
+//
+
+import XCTest
+@testable import Zorro
+
+class ZorroTests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() throws {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
